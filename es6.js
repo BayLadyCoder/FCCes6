bf3cb1f8efdeea3c33f71e187b762ace7dc76ec1
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-09-04 23:14:12 -0400

    Compare Scope of var and let

--- es6.js
+++ es6.js
@@ -41,3 +41,16 @@ function catTalk() {
 
 }
 catTalk();
+
+
+// Compare Scopes of the var and let Keywords
+function checkScope() {
+    "use strict";
+    let i = "function scope";
+    if (true) {
+        let i = "block scope";
+        console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+}
